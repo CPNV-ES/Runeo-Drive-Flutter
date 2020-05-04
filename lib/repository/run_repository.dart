5950729d5b0f87e4317b7f6ac2d80350ee122941
muminovicanel <muminovic____@hotmail.ch>
2020-05-04 15:49:19 +0200

    fixed repository to work in only one file

--- lib/repository/run_repository.dart
+++ lib/repository/run_repository.dart
@@ -1,16 +1,29 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:RuneoDriverFlutter/models/index.dart';
-import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
 
-import 'package:RuneoDriverFlutter/repository/run_api_provider.dart';
+abstract class RunRepository {
+  Future<List<Run>> getRuns();
+}
 
-class RunRepository {
-  final RunApiProvider runApiProvider;
+class RunRepositoryImpl implements RunRepository {
+	final String _baseUrl = 'http://127.0.0.1:8000/api';
+	final String _apiToken = 'VA5PuxHGSYy5FnqX5enz1HUa9DtuQCQNyIl5LTxk5oViPEmGnQIo0Ef3qB1B';
 
-  RunRepository({
-    @required this.runApiProvider
-  }) : assert(runApiProvider != null);
+	Future<List<Run>> getRuns() async {
+		var response = await http.get(
+			'$_baseUrl/runs',
+			headers: {HttpHeaders.authorizationHeader: "Basic $_apiToken"}
+		);
 
-  Future<List<Run>> getRuns() async {
-    return await runApiProvider.getRuns();
-  }
+		if (response.statusCode == 200) {
+			var data = json.decode(response.body);
+			List<Run> runs = Run.fromJson(data).props;
+			return runs;
+		} else {
+			throw Exception('error getting runs');
+		}
+	}
 }
