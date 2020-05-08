655b33b3fce57372dfc50ba53c0e58018c3e3ccd
muminovicanel <muminovic____@hotmail.ch>
2020-05-08 11:58:22 +0200

    fixed identifier of functions and add log out function

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -7,10 +7,11 @@ import 'package:meta/meta.dart';
 abstract class UserRepository {
   Future<User> authenticate();
   Future<User> getCurrentUser();
+  Future<bool> isAuthenticated();
+  Future<void> logOut();
+  Future<String> barcodeScanning();
   Future<void> _deleteFromStorage(String key);
   Future<void> _saveToStorage(String key, dynamic value);
-  Future<bool> _isAuthenticated();
-  Future _barcodeScanning();
 }
 
 class UserRepositoryImpl implements UserRepository {
@@ -35,6 +36,37 @@ class UserRepositoryImpl implements UserRepository {
     return await this.storage.getItem("currentUser");
   }
 
+  Future<bool> isAuthenticated() async {
+    /// read from keystore/keychain
+    var value = await this.storage.getItem("currentUser");
+    if (value != null) {
+      return true;
+    }
+    return false;
+  }
+
+  Future<void> logOut() async {
+    this._deleteFromStorage("token");
+    this._deleteFromStorage("currentUser");
+  }
+
+  Future<String> barcodeScanning() async {
+    try {
+      ScanResult barcode = await BarcodeScanner.scan();
+      return barcode.rawContent;
+    } catch (e) {
+      if (e.code == BarcodeScanner.cameraAccessDenied) {
+        return 'Access to the camera has been denied!';
+      } else {
+        return 'Unknown error: $e';
+      }
+    } on FormatException {
+      return 'Not the right format!';
+    } catch (e) {
+      return 'Unknown error: $e';
+    }
+  }
+
   Future<void> _deleteFromStorage(String key) async {
     /// delete from keystore/keychain
     await this.storage.deleteItem(key);
@@ -43,33 +75,7 @@ class UserRepositoryImpl implements UserRepository {
 
   Future<void> _saveToStorage(String key, dynamic value) async {
     /// write to keystore/keychain
-    await this.storage.setItem("token", value);
+    await this.storage.setItem(key, value);
     return;
-  }
-
-  Future<bool> _isAuthenticated() async {
-    /// read from keystore/keychain
-    String value = await this.storage.getItem("token");
-    if (value != null) {
-      return true;
-    }
-    return false;
-  }
-
-  Future _barcodeScanning() async {
-    try {
-      ScanResult barcode = await BarcodeScanner.scan();
-      return barcode.rawContent;
-    } catch (e) {
-      if (e.code == BarcodeScanner.cameraAccessDenied) {
-        return barcode.type;
-      } else {
-        return 'Unknown error: $e' as ScanResult;
-      }
-    } on FormatException {
-      return barcode.formatNote;
-    } catch (e) {
-      return 'Unknown error: $e';
-    }
   }
 }
