5a8ad77123b5a95cf12be2df3398859f5841a48e
muminovicanel <muminovic____@hotmail.ch>
2020-05-08 11:58:58 +0200

    added login action blog when button log in pressed

---
+++ lib/bloc/login/login_bloc.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:RuneoDriverFlutter/bloc/authentication/index.dart';
+import 'package:RuneoDriverFlutter/repository/user_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:RuneoDriverFlutter/bloc/login/index.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final UserRepositoryImpl _userRepository;
+  final AuthenticationBloc _authenticationBloc;
+
+  LoginBloc({
+    @required AuthenticationBloc authenticationBloc,
+    @required UserRepositoryImpl userRepository,
+  })  : assert(userRepository != null),
+        assert(authenticationBloc != null),
+        _authenticationBloc = authenticationBloc,
+        _userRepository = userRepository;
+  
+  @override
+  Future<void> close() async{
+    // dispose objects
+    await super.close();
+  }
+
+  @override
+  LoginState get initialState => LoginInitial();
+
+  @override
+  Stream<LoginState> mapEventToState(LoginEvent event) async* {
+    if (event is LoginInButtonPressed) {
+      yield LoginLoading();
+
+      try {
+        final user = await _userRepository.authenticate(
+          key: event.token
+        );
+        if (user != null) {
+          // push new authentication event
+          _authenticationBloc.add(LoggedIn(user: user));
+          yield LoginSuccess();
+          yield LoginInitial();
+        } else {
+          yield LoginFailure(error: 'Something very weird just happened');
+        }
+        yield LoginInitial();
+      } catch (error) {
+        yield LoginFailure(error: error.message ?? 'An unknown error occured');
+      }
+    }
+  }
+}
