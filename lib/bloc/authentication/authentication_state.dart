edb30d44d2d2a12ec9924d20e12d001d952581d5
muminovicanel <muminovic____@hotmail.ch>
2020-05-08 11:14:42 +0200

    created gobal file that exports authentication bloc files

--- lib/bloc/authentication/authentication_state.dart
+++ lib/bloc/authentication/authentication_state.dart
@@ -8,13 +8,13 @@ abstract class AuthenticationState extends Equatable {
   List<Object> get props => [];
 }
 
-class AuthenticationInitial extends AuthenticationState {}
-
-class AuthenticationLoading extends AuthenticationState {}
+class AuthenticationUninitialized extends AuthenticationState {}
 
 class AuthenticationUnauthenticated extends AuthenticationState {}
 
 class AuthenticationAuthenticated extends AuthenticationState {}
+
+class AuthenticationLoading extends AuthenticationState {}
 
 class AuthenticationFailure extends AuthenticationState {
   final String message;
