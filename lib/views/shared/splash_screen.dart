d13344df1cd820b527418dd1ce101c5c1d89d3fc
muminovicanel <muminovic____@hotmail.ch>
2020-05-13 11:55:53 +0200

    add logo to splash screen

--- lib/views/shared/splash_screen.dart
+++ lib/views/shared/splash_screen.dart
@@ -5,7 +5,18 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Runeo Drive'),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 100.0),
+            Text('Runeo Drive', style: TextStyle(fontSize: 40.0)),
+            Image.asset(
+              'assets/logo.png', 
+              height: 150,
+              width: 100,
+              fit: BoxFit.fitWidth
+            ),
+          ],
+        ),
       ),
     );
   }
