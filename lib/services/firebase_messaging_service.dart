34aa963ba338d00b85b242c554b1fcbe90f8a949
muminovicanel <muminovic____@hotmail.ch>
2020-06-03 12:50:36 +0200

    add un/subscribe to a topic functions

--- lib/services/firebase_messaging_service.dart
+++ lib/services/firebase_messaging_service.dart
@@ -6,8 +6,6 @@ class FirebaseMessagingService {
   FirebaseMessagingService._internal() {
     // save the client so that it can be used else where
     _firebaseMessaging = FirebaseMessaging();
-    // setup listeners
-    firebaseCloudMessagingListeners();
   }
 
   static final FirebaseMessagingService _instance =
@@ -22,26 +20,12 @@ class FirebaseMessagingService {
   // getter for firebase messaging client
   get firebaseMessaging => _firebaseMessaging;
 
+  /// Get firebase token
   Future<String> getToken() async {
     return await _firebaseMessaging.getToken();
   }
 
-  void firebaseCloudMessagingListeners() {
-    if (Platform.isIOS) getIOSPermission();
-
-    _firebaseMessaging.configure(
-      onMessage: (Map<String, dynamic> message) async {
-        print('on message $message');
-      },
-      onResume: (Map<String, dynamic> message) async {
-        print('on resume $message');
-      },
-      onLaunch: (Map<String, dynamic> message) async {
-        print('on launch $message');
-      },
-    );
-  }
-
+  /// Ask for permission on IOS
   void getIOSPermission() {
     _firebaseMessaging.requestNotificationPermissions(
         IosNotificationSettings(sound: true, badge: true, alert: true));
@@ -49,5 +33,19 @@ class FirebaseMessagingService {
         .listen((IosNotificationSettings settings) {
       print("Settings registered: $settings");
     });
+  }
+
+  /// Subscribe to a topic
+  /// 
+  /// Take [topic] as an argument
+  void firebaseSubscribe(String topic) {
+    _firebaseMessaging.subscribeToTopic(topic);
+  }
+
+  /// Unsubscribe to a topic
+  /// 
+  /// Take [topic] as an argument
+  void firebaseUnsubscribe(String topic) {
+    _firebaseMessaging.unsubscribeFromTopic(topic);
   }
 }
